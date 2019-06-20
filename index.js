df04e1195516696c51b1b879e982da9b4a60bec3
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 16:05:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function shout(string) {
 return string.toUpperCase()
 }
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("hello!")
+  return (string.toLowerCase)
+}
