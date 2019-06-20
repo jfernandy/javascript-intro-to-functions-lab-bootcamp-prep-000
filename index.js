9e6614df673452f4a3ec909a3b82d8d2d256363d
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 15:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function logWhisper(string) {
   console.log('hello')
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string)
+  return ("I can't hear you!")
+}
