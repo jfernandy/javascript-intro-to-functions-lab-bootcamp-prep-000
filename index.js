ad2f24a1bede95e58e1e7cdde809ad4f1ee82a1c
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 15:55:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function logWhisper(string) {
 var lowercase = 'hello!'
 
 function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase === lowercase)
+  if {lowercase.toLowerCase === lower
   return ("I can't hear you!")
 }
