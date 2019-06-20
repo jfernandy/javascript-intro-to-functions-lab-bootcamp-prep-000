cc474eb68cdaa22f257c870a9ef6b144462a8b32
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 16:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ var lowercase = "hello!"
 function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase)
   return ("I can't hear you!")
-}
-
-function sayHiToGrandma(string) {
-  if (uppercase.toUpperCase() === uppercase)
+  else if (uppercase.toUpperCase() === uppercase)
   return ("YES INDEED!")
 }
+
